b8510e986c5097b301c8aa1b63d656eb735bca5a
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2023-06-22 16:48:32 +0100

    Calculate NRM stats

--- report/interactive graphs 4b - asylum accommodation.R
+++ report/interactive graphs 4b - asylum accommodation.R
@@ -2,5 +2,7 @@ library(tidyverse)
 library(asylum)
 
 # ---- How many people are receiving section 95 support including accommodation (not subs only) ----
+# Answered in (4a)
 
 # ---- What type of accommodation are people in? Initial accommodation, contingency (hotels), dispersal, barge, barracks, HMOs? ----
+# Answered in (4a)
--- report/interactive graphs 5 - trafficking.R
+++ report/interactive graphs 5 - trafficking.R
@@ -2,6 +2,53 @@ library(tidyverse)
 library(asylum)
 
 # ---- How many people have been referred into the NRM in the last 12 months by nationality, age and setting of referral? ----
+nrm_referrals <- 
+  bind_rows(
+  asylum::nrm_referrals_2023_q1,
+  asylum::nrm_referrals_2022_q4,
+  asylum::nrm_referrals_2022_q3,
+  asylum::nrm_referrals_2022_q2
+)
+
+# By first responder
+nrm_referrals |> 
+  filter(`Exploitation type` == "Total" & `Age at exploitation` == "Total" & Gender == "Total" & Nationality == "Total") |> 
+  filter(
+    (`First responder type` == "Government agency" & `First responder` != "Government agency total") |
+    (`First responder type` != "Government agency" & str_detect(`First responder`, "total"))
+  ) |> 
+  
+  mutate(`First responder type` = if_else(`First responder type` == "Government agency", `First responder`, `First responder type`)) |> 
+  
+  group_by(`First responder type`) |> 
+  summarise(People = sum(People)) |> 
+  
+  write_csv("data-raw/flourish/5 - Trafficking/NRM referrals by first responder.csv")
+
+# By age and sex
+nrm_referrals |> 
+  filter(`Age at exploitation` != "Total" & Nationality == "Total") |> 
+  filter(Gender != "Total") |> 
+  
+  group_by(`Age at exploitation`, Gender) |> 
+  summarise(People = sum(People)) |> 
+  
+  pivot_wider(names_from = Gender, values_from = People) |> 
+  
+  write_csv("data-raw/flourish/5 - Trafficking/NRM referrals by age and gender.csv")
+
+# By nationality
+nrm_referrals |> 
+  filter(Nationality != "Total") |> 
+  filter(str_detect(`First responder`, "total")) |> 
+  
+  group_by(Nationality) |> 
+  summarise(People = sum(People)) |> 
+  ungroup() |> 
+  
+  arrange(desc(People)) |> 
+  
+  write_csv("data-raw/flourish/5 - Trafficking/NRM referrals by nationality.csv")
 
 # ---- How many people received a positive reasonable grounds decision by age, gender, nationality and setting of referral? ----
 
