e84de395a941a39ebdcdfe8669abcf9f932f999f
mkugan <131258713+mkugan@users.noreply.github.com>
2023-07-06 15:13:45 +0100

    Update What is the cost of productivity.R

--- report/Report Plots by Question/What is the cost of productivity.R
+++ report/Report Plots by Question/What is the cost of productivity.R
@@ -13,17 +13,35 @@ asylum_costs_and_productivity |>
   labs(title = "Total Asylum System Cost", x = "Financial Year", y = "Cost")
 
 
+ggplot(asylum_costs_and_productivity) +
+  geom_col(aes(x = `Financial Year`, y = `Total Asylum Costs`), colour = brc_colours$red_dunant, fill = brc_colours$red_dunant) +
+  theme_classic() +
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
+  labs(title = "Cost of the Asylum System", 
+       subtitle = "In 2022, the cost of the system reached 2 billion.",
+       x = NULL,
+       y = " Total Cost",
+       caption = "BRC Analyses of HO Data, March 2023")
+
 #Productivity 
 Productivity <- asylum_costs_and_productivity %>%
   select(`Financial Year`, Productivity) %>%
   filter(`Financial Year` > "2010/11")
 
-Productivity |>
+ProductivityGraph <- (Productivity |>
   ggplot(aes(`Financial Year`, Productivity), group = 1) +
   geom_line(colour = "red", group = 1) +
   geom_point(alpha = 0.5, colour = "red") +
   theme_classic() +
-  labs(title = "Productivity of Asylum System", 
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
+  labs(title = "Productivity of Asylum Case Workers", 
+       subtitle = "Productivity of home office measured as the average number of cases complted by asylum decision makers per month each year", 
+       x = NULL,
        y = "Productivity", 
-       caption = "BRC Analyses of HO Data, March, 2023")
+       caption = "BRC Analyses of HO Data, March, 2023"))
 
+ProductivityGraph + geom_text(aes(label = scales::comma(Productivity)), show.legend = FALSE, size = rel(2)) 
+
+ProductivityGraph + geom_vline(xintercept = 2016/17, colour = "black")
+
+#check why geomvline isnt working with Matt or bring it up with Lydia for design
