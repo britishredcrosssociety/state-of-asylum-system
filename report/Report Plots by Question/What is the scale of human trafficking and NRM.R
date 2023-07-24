b9f002dd1922f528dc719bb5a2527f7242b8ac3d
mkugan <131258713+mkugan@users.noreply.github.com>
2023-07-24 15:53:50 +0100

    Update What is the scale of human trafficking and NRM.R

--- report/Report Plots by Question/What is the scale of human trafficking and NRM.R
+++ report/Report Plots by Question/What is the scale of human trafficking and NRM.R
@@ -9,83 +9,88 @@ nrm_referrals <-
     asylum::nrm_referrals_2022_q2
   )
 
-ByFirstResponder <- nrm_referrals |> 
-  filter(`Exploitation type` == "Total" & `Age at exploitation` == "Total" & Gender == "Total" & Nationality == "Total") |> 
-  filter(
-    (`First responder type` == "Government agency" & `First responder` != "Government agency total") |
-      (`First responder type` != "Government agency" & str_detect(`First responder`, "total"))
-  ) |> 
-  
-  mutate(`First responder type` = if_else(`First responder type` == "Government agency", `First responder`, `First responder type`)) |> 
-  
-  
-  group_by(`First responder type`) |> 
-  summarise(People = sum(People)) 
-
-
-#NRM by group#
+view(nrm_referrals %>%
+  group_by(Nationality) %>%
+  summarise(Total = sum(People)))
 
 NRMTotal <- nrm_referrals %>%
-  group_by(`First responder`, `Exploitation type`, `Age at exploitation`, Gender, Nationality) %>%
+  group_by(`Age at exploitation`, Gender, Nationality) %>%
   summarise(Total = sum(People))
 
-#NRM by Age in 2022/23#
+NRMTotal <- NRMTotal %>%
+  filter(`Age at exploitation` != "Total") %>%
+  filter(Gender != "Total") %>%
+  filter(Nationality != "Total")
 
-NRMAge <- nrm_referrals |> 
-  filter(`Age at exploitation` != "Total" & Nationality == "Total") |> 
-  filter(Gender != "Total") |> 
-  
-  group_by(`Age at exploitation`, Gender) |> 
-  summarise(Total = sum(People)) 
+#"Total" was removed from each category as it was grouped for other analyses.
 
-NRMAge |>
-  ggplot(aes(fill = `Age at exploitation`, y = Total, x = Gender)) +
-  geom_bar(position ="stack", stat="identity") + 
+#NRM referral by Sex and Age#
+
+NRMTotal %>%
+  filter(Gender != "Not Recorded") %>%
+  filter(Gender != "Other") %>%
+  group_by(`Age at exploitation`, Gender) %>%
+  summarise(TotalSum = sum(Total)) %>%
+  ggplot(aes(fill = `Age at exploitation`, x =  Gender,y =  TotalSum)) +
+  geom_bar(position = "stack", stat ="identity") +
   theme_classic() +
-  labs(title = "NRM Referrals by Age and Sex, 2022/23", 
+  labs(title = "Referrals to the National Referral Mechanism (NRM) by age and sex, 2022/23", 
        x = "Sex", 
-       y = "Number of Referrals") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA))
+       y = "Number of Referrals",
+       caption = "British Red Cross analysis of Home Office data, April 2022 to January 2023") +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 10000)) +
+  scale_fill_manual(values = c(brc_colours$red_light,
+                               brc_colours$red_dunant,
+                               brc_colours$red_deep))
+ 
+#NRM by Top Nationalities 
 
-#NRM by Nationality- tO REVISE ON HOW TO SHOW. 
+NRMTotal %>%
+  filter(Nationality == "UK") %>%
+  group_by(Nationality) %>%
+  summarise(UK = sum(Total))
 
-nrm_referrals <- nrm_referrals |> 
-  filter(Nationality != "Total") |> 
-  filter(str_detect(`First responder`, "total")) |> 
-  
-  group_by(Nationality) |> 
-  summarise(People = sum(People)) |> 
-  ungroup() |> 
-  
-  arrange(desc(People)) 
+#1775 male children/ 2469 were referred to NRM- mostly male children from UK.
 
-nrm_referrals |>
-  ggplot(aes(x = Nationality, y = People)) +
-  geom_point(alpha = 0.5)
+NRMTotal %>%
+  group_by(Nationality) %>%
+  summarise(TotalNat = sum(Total)) %>%
+  filter(Nationality != "UK") %>%
+  filter(TotalNat > 300) %>%
+  ggplot(aes(x = Nationality, y = TotalNat)) +
+  geom_col(fill = brc_colours$red_dunant, colour = brc_colours$red_dunant) +
+  geom_text(aes(label = scales::comma(TotalNat)), show.legend = FALSE, size = rel(3)) +
+  theme_classic() +
+  labs(title = "Nationalities aside from the United Kingdom with the highest referrals to the National Referral Mechanism (NRM) in 2022/23",
+       subtitle = "2462 individuals from the United Kingdom were referred to the NRM",
+       x = "Nationality",
+       y = "Number of Referrals",
+       caption = "British Red Cross analysis of Home Office data, April 2022 to January 2023") +
+  theme(axis.text.x = element_text(angle = 80, vjust = 0.5, hjust=0.5)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 5000))
 
+#Positive Conclusive Grounds 
 PositiveConclusive <- nrm_conclusive_grounds %>%
   select(Year, Quarter, `Adult (18 or over) - Positive conclusive grounds`, `Child (17 or under) - Positive conclusive grounds`, `Age not specified or unknown - Positive conclusive grounds`) 
 
-PC <- PositiveConclusive %>%
+PositiveConclusive %>%
   filter(Quarter == "Total") |>
   ggplot(aes(x = Year)) +
-  geom_line(aes(x = Year, y = `Adult (18 or over) - Positive conclusive grounds`), colour = "red", show.legend = TRUE) +
-  geom_line(aes(x = Year, y = `Child (17 or under) - Positive conclusive grounds`), colour = "blue", show.legend = TRUE) +
-  geom_line(aes(x = Year, y = `Age not specified or unknown - Positive conclusive grounds`), colour = "black", show.legend = TRUE) +
+  geom_line(aes(x = Year, y = `Adult (18 or over) - Positive conclusive grounds`), colour = brc_colours$red_dunant) +
+  geom_line(aes(x = Year, y = `Child (17 or under) - Positive conclusive grounds`), colour = brc_colours$teal) +
+  geom_line(aes(x = Year, y = `Age not specified or unknown - Positive conclusive grounds`), colour = brc_colours$black_shadow) +
   theme_classic() +
-  labs(title = "Number of People who Recieved Positive Conclusive Grounds", 
+  labs(title = "Number of people who recieved positive conclusive grounds by age",
+       subtitle = "Adults shown in red, children in teal and unknown in black",
        x = "Year", 
-       y = "Number of People") +
+       y = "Number of People",
+       colour = "Age",
+       caption = "British Red Cross analysis of Home Office data, January 2014 until January 2023") +
   scale_x_continuous(breaks = c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
-  
-  PC + scale_color_manual(name = "Age Groups", 
-                          values = (c("Adult (18 or over) - Positive conclusive grounds" = "red", 
-                                      "Child (17 or under) - Positive conclusive grounds" = "blue", 
-                                      "Age not specified or unknown - Positive conclusive grounds" = "black"))) + 
-  labs(color = 'Age Groups')
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) 
 
-#To fix legend 
+#To discuss with Matt why the legend is coming up with colour inside aes() but does not apply when inside, but legend shows up.
+
 
 #NRM Reasonable Grounds# 
 
@@ -94,25 +99,20 @@ view(nrm_reasonable_grounds)
 ReasonableGrounds <- nrm_reasonable_grounds %>%
   select(Year, Quarter, `Adult (18 or over) - Positive reasonable grounds`, `Child (17 or under) - Positive reasonable grounds`, `Age not specified or unknown - Positive reasonable grounds`) 
 
-RGPlot <- ReasonableGrounds %>%
+ReasonableGrounds %>%
   filter(Quarter == "Total") |>
   ggplot(aes(x = Year)) +
-  geom_line(aes(x = Year, y = `Adult (18 or over) - Positive reasonable grounds`), colour = "red") +
-  geom_line(aes(x = Year, y = `Child (17 or under) - Positive reasonable grounds`), colour = "blue") +
-  geom_line(aes(x = Year, y = `Age not specified or unknown - Positive reasonable grounds`), colour = "black") +
+  geom_line(aes(x = Year, y = `Adult (18 or over) - Positive reasonable grounds`), colour = brc_colours$red_dunant) +
+  geom_line(aes(x = Year, y = `Child (17 or under) - Positive reasonable grounds`), colour = brc_colours$teal) +
+  geom_line(aes(x = Year, y = `Age not specified or unknown - Positive reasonable grounds`), colour = brc_colours$black_shadow) +
   theme_classic() +
-  labs(title = "Number of People who Recieved Positive Reasonable Grounds", 
+  labs(title = "Number of people who recieved positive reasonable grounds by age", 
+       subtitle = "Adults shown in red, children in teal and unknown in black",
        x = "Year", 
-       y = "Number of People") +
+       y = "Number of People",
+       caption = "British Red Cross analysis of Home Office data, January 2014 until January 2023") +
   scale_x_continuous(breaks = c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
-  
-  RGPlot <- scale_colour_manual("", 
-                                breaks = c("Adult (18 or over) - Positive reasonable grounds", "Child (17 or under) - Positive reasonable grounds", "Age not specified or unknown - Positive reasonable grounds"),
-                                values = c("red", "green", "blue")) 
-xlab(" ") +
-  scale_y_continuous("Temperatura (C)", limits = c(-10,40)) + 
-  labs(title="TITULO")
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) 
 
 #Fix the legend
 
@@ -136,13 +136,15 @@ DTNTotal <- DTNTotal %>%
   summarise(Total2 = sum(Total))
 
 DTNTotal %>%
-  filter(Total2 > 100) %>%
-  arrange(desc(Total2)) %>%
+  filter(Nationality != "UK") %>%
+  filter(Total2 > 200) %>%
   ggplot(aes(Nationality, Total2)) +
-  geom_point(aes(size = Total2, alpha = 0.5, colour = "red"), show.legend = FALSE) +
+  geom_col(fill = brc_colours$red_dunant, colour = brc_colours$red_dunant) +
   geom_text(aes(label = scales::comma(Total2)), show.legend = FALSE, size = rel(3)) +
   theme_classic() +
-  labs(title = "Duty to Notitify by Nationalities (Top 10)", 
+  labs(title = "Nationaities with the highest number of individuals referred to Duty to Notitify aside from the United Kingdom",
+       subtitle = "406 individuals from the United Kingdom were referred to Duty to Notify from April 2022 to Janaury 2023",
        x = "Nationalities", 
-       y = "Number of People") +
+       y = "Number of People", 
+       caption = "British Red Cross analysis of Home Office data, April 2022 - January 2023") +
   scale_y_continuous(labels = scales::comma, limits = c(0, NA))
