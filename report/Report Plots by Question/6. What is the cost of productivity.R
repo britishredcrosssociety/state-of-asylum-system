057369001d9552848f698b0c26bb037116fd31c3
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2023-08-02 16:54:03 +0100

    Aesthetic tweaks to charts

--- report/Report Plots by Question/6. What is the cost of productivity.R
+++ report/Report Plots by Question/6. What is the cost of productivity.R
@@ -9,15 +9,16 @@ source("report/theme_brc.R")
 view(asylum_costs_and_productivity) 
 
 # ---- Cost of the System ---- 
-ggplot(asylum_costs_and_productivity) +
-  geom_col(aes(x = `Financial Year`, y = `Total Asylum Costs`), colour = brc_colours$red_dunant, fill = brc_colours$red_dunant) +
-  geom_text(aes(x = `Financial Year`, y = `Total Asylum Costs`, label = scales::comma(`Total Asylum Costs`)), vjust = -0.5, show.legend = FALSE, size = rel(3)) +
-  theme_classic() +
-  scale_y_continuous(labels = scales::unit_format(unit = "B", scale = 1e-9)) +
-  labs(title = "Cost of the asylum system, 2010/11 to 2021/22", 
-       subtitle = "In 2022, the cost of the system surpassed 2 billion.",
-       x = "Financial Year",
-       y = " Total Cost in Billions (£)",
+ggplot(asylum_costs_and_productivity, aes(x = `Financial Year`, y = `Total Asylum Costs`)) +
+  geom_col(colour = brc_colours$red_dunant, fill = brc_colours$red_dunant) +
+  geom_text(aes(label = scales::dollar(`Total Asylum Costs`, prefix = "£")), vjust = -0.5, show.legend = FALSE, size = rel(3)) +
+  theme_brc() +
+  # scale_y_continuous(labels = scales::unit_format(unit = "B", scale = 1e-9)) +
+  scale_y_continuous(labels = scales::dollar_format(prefix = "£", suffix = "B", scale = 1e-9, accuracy = 0.1), limit = c(0, 2500000000), expand = c(0, NA)) +
+  labs(title = "In 2022, the cost of the system surpassed £2 billion.",
+       subtitle = "Cost of the asylum system, 2010/11 to 2021/22", 
+       x = "Financial year",
+       y = " Total cost (£ billions)",
        caption = "British Red Cross analysis of Home Office data, from financial year 2010/11 to 2021/22")
 
 # ---- Productivity ---- 
@@ -27,11 +28,11 @@ Productivity <- asylum_costs_and_productivity %>%
 
 Productivity |>
   ggplot(aes(`Financial Year`, Productivity), group = 1) +
-  geom_line(colour = "red", group = 1) +
-  geom_point(alpha = 0.5, colour = "red") +
+  geom_line(colour = brc_colours$red_dunant, group = 1) +
+  geom_point(alpha = 0.5, colour = brc_colours$red_dunant) +
   geom_text(aes(label = scales::comma(Productivity)), show.legend = FALSE, size = rel(3)) +
-  theme_classic() +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
+  theme_brc() +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 20), expand = c(0, NA)) +
   labs(title = "Asylum caseworker productivity from 2011/12 to 2021/22", 
        subtitle = "Average number of substantive interviews or decisions completed by asylum caseworkers each month",
        x = "Financial Year",
