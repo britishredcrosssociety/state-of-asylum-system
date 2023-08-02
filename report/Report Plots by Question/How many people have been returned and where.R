375a7cb450791401d77d6bd340aa43e03a779c01
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2023-08-02 15:50:55 +0100

    BRC style for graphs

--- report/Report Plots by Question/How many people have been returned and where.R
+++ report/Report Plots by Question/How many people have been returned and where.R
@@ -13,15 +13,15 @@ ReturnTotal <- returns %>%
 
 ReturnTotal |>
   ggplot(aes(Year, TotalReturn)) +
-  geom_line(aes(colour = "red"), show.legend = NULL) +
+  geom_line(aes(colour = "red"), show.legend = FALSE) +
   geom_text(aes(label = scales::comma(TotalReturn)), show.legend = FALSE, size = rel(3)) +
   scale_x_continuous(breaks = c(2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)) +
   scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
-  theme_classic() +
-  labs(title = 'Total Numbers of Returns per Year', 
-       x = 'Date', 
-       y = 'Number of Returns', 
-       caption = 'BRC Analysis of UK Home Office Data. Mock analyses')
+  theme_brc() +
+  labs(title = 'Total number of returns per year', 
+       x = NULL, 
+       y = 'Number of returns', 
+       caption = 'British Red Cross analysis of UK Home Office data. Mock analyses')
 
 # Returns Quarterly
 ReturnsQ <- returns %>%
@@ -32,14 +32,14 @@ ReturnsQ <- returns %>%
 ReturnsQ %>%
   filter(Year > 2021) |>
   ggplot(aes(Quarter, TotalQReturn)) +
-  geom_line(aes(colour = "red"), show.legend = NULL) +
+  geom_line(aes(colour = "red"), show.legend = FALSE) +
   geom_text(aes(label = scales::comma(TotalQReturn)), show.legend = FALSE, size = rel(3)) +
   scale_x_continuous(breaks = c(1, 2, 3, 4)) +
   scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
-  theme_classic() +
-  labs(title = 'Total Numbers of Returns per Quarter', 
+  theme_brc() +
+  labs(title = 'Total numbers of returns per quarter', 
        x = 'Quarter', 
-       y = 'Number of Returns', 
-       caption = 'BRC Analysis of UK Home Office Data. Mock analyses')
+       y = 'Number of returns', 
+       caption = 'British Red Cross analysis of UK Home Office data. Mock analyses')
 
 #  Can examine what nationalities we see at different quarters, but may just be a better question to pose to team.# 
