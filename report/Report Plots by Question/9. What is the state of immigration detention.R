8200af37cca0a2b016a463b0de177d6bbb89cb28
mkugan <131258713+mkugan@users.noreply.github.com>
2023-08-04 10:45:25 +0100

    minor edits

--- report/Report Plots by Question/9. What is the state of immigration detention.R
+++ report/Report Plots by Question/9. What is the state of immigration detention.R
@@ -6,23 +6,23 @@ source("report/theme_brc.R")
 # ---- QUESTION: WHAT IS THE STATE OF DETENTION?
 
 # ---- People in Detention ----
-OverallinDetention <- people_in_detention %>%
-  select(Year, Age, Sex, `Length of detention`, People) %>%
-  group_by(Year) %>%
+OverallinDetention <- people_in_detention |>
+  select(Year, Age, Sex, `Length of detention`, People) |>
+  group_by(Year) |>
   summarise(TotalinDetention = sum(People))
 
 OverallinDetention |>
   ggplot(aes(Year, TotalinDetention)) +
-  geom_line(aes(colour = "red"), show.legend = FALSE) +
-  geom_point(aes(colour = "red", alpha = 0.5), show.legend = FALSE) +
-  geom_text(aes(label = scales::comma(TotalinDetention)), show.legend = FALSE, size = rel(2)) +
-  theme_classic() +
+  geom_line(aes(colour = brc_colours$red_dunant), show.legend = FALSE) +
+  geom_point(aes(colour = brc_colours$red_dunant, alpha = 0.5, size = TotalinDetention), show.legend = FALSE) +
+  geom_text(aes(label = scales::comma(TotalinDetention)), show.legend = FALSE, size = rel(3)) +
+  theme_brc() +
   labs(title = "Number of people in immigration detention from 2010 to 2023",
        x = "Year",
        y = "Number of People", 
        caption = "British Red Cross Analysis of Home Office data, March 2010 to March 2023") +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 15000))
+  scale_x_continuous(breaks = c(2010 : 2023)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 15000), expand = c(0,NA))
 
 # ---- Length of Time in Detention ----
 detention_length <- 
@@ -48,21 +48,21 @@ detention_length <-
 detention_length |> 
   ggplot(aes(fill =`Length of detention`, x = Year, y = People)) +
   geom_bar(position = "stack", stat = "identity") +
-  theme_classic() +
-  labs(title = "Length of time people have spent in immigration detention from 2010 - 2023",
+  theme_brc() +
+  labs(title = "Length of time people have spent in immigration detention from 2010 to 2023",
        x = "Year",
-       y = "Number of People",
-       caption = "British Red Cross analysis of Home Office data, March 2010 to 2023") +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 40000)) +
-  scale_fill_manual(values = c(brc_colours$red_light,
+       y = "Number of people",
+       caption = "British Red Cross analysis of Home Office data, March 2010 to March 2023") +
+  scale_x_continuous(breaks = c(2010:2023)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 40000), expand = c(0,NA)) +
+  scale_fill_manual(values = c(brc_colours$red_earth,
                                brc_colours$red_dunant,
-                               brc_colours$red_deep))
+                               brc_colours$red_light))
 
 # ---- Detention: Age and Sex ---- 
-AgeSexDetention <- people_in_detention %>%
-  select(Year, Age, Sex, People) %>%
-  group_by(Year, Age, Sex) %>%
+AgeSexDetention <- people_in_detention |>
+  select(Year, Age, Sex, People) |>
+  group_by(Year, Age, Sex) |>
   summarise(AgeTotal = sum(People))
 
 AgeSexDetention$Age <- factor(AgeSexDetention$Age, levels=c('70 and over',
@@ -71,63 +71,65 @@ AgeSexDetention$Age <- factor(AgeSexDetention$Age, levels=c('70 and over',
                                                             '18 to 29',
                                                             '17 and under'))
                                                                 
-AgeSexDetention %>%
-  filter(Age != "NA") %>%
+AgeSexDetention |>
+  filter(Age != "NA") |>
   ggplot(aes(fill = Age, Year, AgeTotal)) +
   geom_bar(position = "stack", stat = "identity") +
-  theme_classic() +
+  theme_brc() +
   labs(title = "Age of people in immigration detention from 2010 to 2023",
        x = "Year",
        y = "Number of People",
        caption = "British Red Cross analysis of Home Office data, March 2010 to March 2023") +
   scale_y_continuous(labels = scales::comma, limits = c(0, 15000)) +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_fill_manual(values = c(brc_colours$red_deep,
-                               brc_colours$red_earth,
+  scale_x_continuous(breaks = c(2010:2023)) +
+  scale_fill_manual(values = c(brc_colours$teal,
+                               brc_colours$steel,
+                               brc_colours$red_light,
                                brc_colours$red_dunant,
-                               brc_colours$red_light))
+                               brc_colours$red_deep
+                               ))
                                
 # ---- Detention Pregnant Women ----
-detention_pregnant_women %>%
-  select(Year, `Number of pregnant women detained in the immigration detention estate`) %>%
-  group_by(Year) %>%
+detention_pregnant_women |>
+  select(Year, `Number of pregnant women detained in the immigration detention estate`) |>
+  group_by(Year) |>
   summarise(Total = sum(`Number of pregnant women detained in the immigration detention estate`)) |>
   ggplot(aes(Year, Total)) +
   geom_line(aes(colour = brc_colours$red_dunant), show.legend = FALSE) +
   geom_point(aes(colour = brc_colours$red_dunant), alpha = 0.5, show.legend = FALSE) +
   geom_text(aes(label = scales::comma(Total)), show.legend = FALSE, size = rel(3)) +
-  theme_classic() +
-  labs(title = "Number of pregnant women in immigration detention from 2016 to March 2023",
+  theme_brc() +
+  labs(title = "Number of pregnant people in immigration detention from 2016 to March 2023",
        x = "Year", 
-       y = "Number of Pregnant People", 
+       y = "Number of pregnant people", 
        caption = "British Red Cross analysis of Home Office data, July 2016 to March, 2023") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA)) +
-  scale_x_continuous(breaks = c(2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) 
+  scale_y_continuous(labels = scales::comma, limits = c(0, 60), expand = c(0,NA)) +
+  scale_x_continuous(breaks = c(2016:2023)) 
 
 # ---- Children in Detention ----
-ChildrenDetention <- people_in_detention %>%
-  filter(Age == "17 and under") %>%
-  group_by(Year) %>%
-  summarise(Total = sum(People)) 
+#ChildrenDetention <- people_in_detention |>
+  #filter(Age == "17 and under") |>
+  #group_by(Year) |>
+  #summarise(Total = sum(People)) 
   
-ChildrenDetention %>%
-  ggplot(aes(Year, Total)) +
-  geom_col(colour = brc_colours$red_dunant, fill = brc_colours$red_dunant)
+#ChildrenDetention |>
+  #ggplot(aes(Year, Total)) +
+  #geom_col(colour = brc_colours$red_dunant, fill = brc_colours$red_dunant)
 
 # using children entering detention as a more accurate depiction rather than in detention as the data is not recorded as well? 
 view(children_entering_detention)
 
-children_entering_detention %>%
+children_entering_detention |>
   ggplot(aes(`Date of entry to detention`, `Total children`)) +
   geom_col(aes(fill = brc_colours$red_dunant, colour = brc_colours$red_dunant), show.legend = FALSE) +
-  geom_text(aes(label = scales::comma(`Total children`)), show.legend = FALSE, size = rel(2), position = position_dodge(width=1), vjust=-0.25) +
-  theme_classic() +
+  geom_text(aes(label = scales::comma(`Total children`)), show.legend = FALSE, size = rel(3), position = position_dodge(width=1), vjust=-0.25) +
+  theme_brc() +
   labs(title = "Number of children entering immigration detention from 2010 to 2022",
        x = "Year",
        y = "Number of Children",
        caption = "British Red Cross analysis of Home office data from March 2010 to March 2022") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 600)) +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)) 
+  scale_y_continuous(labels = scales::comma, limits = c(0, 600), expand = c(0,NA)) +
+  scale_x_continuous(breaks = c(2010:2022)) 
 
 # Check with team about children IN and children entering detention and why these numbers are so different?!
 
@@ -144,16 +146,16 @@ people_leaving_detention |>
   summarise(Total = sum(Leaving)) %>%
   ggplot(aes(fill = `Reason for leaving detention`, x = Year, y = Total)) +
   geom_bar(position = "stack", stat = "identity") +
-  theme_classic() +
+  theme_brc() +
   labs(title = "Number of people leaving detention from 2010 to 2023",
        subtitle = str_wrap("Bailed includes bailed by immigration judge and Secretary of State. Other is defined as people returned to criminal detention, released unconditionally, those sectioned under the Mental Health Act, deaths, absconds and reasons unavailable when leaving detention"),
        x = "Year",
-       y = "Number of People",
+       y = "Number of people",
        caption = "British Red Cross analysis of Home Office data, March 2010 to March 2023") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 40000)) +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)) +
-  scale_fill_manual(values = c(brc_colours$red_light,
-                               brc_colours$red_mercer,
+  scale_y_continuous(labels = scales::comma, limits = c(0, 40000), expand = c(0,NA)) +
+  scale_x_continuous(breaks = c(2010:2023)) +
+  scale_fill_manual(values = c(brc_colours$steel,
+                               brc_colours$teal,
                                brc_colours$red_earth,
                                brc_colours$red_dunant))
   
@@ -169,7 +171,7 @@ people_in_detention %>%
   group_by(Age) %>%
   ggplot(aes(fill = Age, x = Year, y = People)) +
   geom_bar(position = "stack", stat = "identity") +
-  theme_classic() +
+  theme_brc() +
   scale_fill_manual(values = c(brc_colours$red_deep,
                               brc_colours$red_earth,
                               brc_colours$red_dunant,
@@ -177,7 +179,7 @@ people_in_detention %>%
                               brc_colours$red_mercer)) +
   labs(title = "Number of women in immigration detention from 2010 to 2023",
        x = "Year",
-       y = "Number of Women",
+       y = "Number of women",
        caption = "British Red Cross analysis of Home Office data, year ending March 2023") +
   scale_y_continuous(labels = scales::comma, limits = c(0, 1500)) +
-  scale_x_continuous(breaks = c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023))
+  scale_x_continuous(breaks = c(2010:2023))
