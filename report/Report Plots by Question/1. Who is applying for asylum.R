66b1c057400794dc93702f804a3eca7f14a77038
mkugan <131258713+mkugan@users.noreply.github.com>
2023-08-30 14:22:14 +0100

    Updated for Q2 data

--- report/Report Plots by Question/1. Who is applying for asylum.R
+++ report/Report Plots by Question/1. Who is applying for asylum.R
@@ -7,29 +7,37 @@ source("report/theme_brc.R")
 
 # ---- 1. Total Asylum Applications ----
 total_applications <- applications |>
-  group_by(Year) |>
-  summarise(Total = sum(Applications))
+  group_by(Date) |>
+  summarise(Total = sum(Applications)) |> 
+  ungroup() |> 
 
-view(total_applications)
+  arrange(desc(Date))
 
-total_applications |>
-  ggplot(aes(Year, Total)) +
+total_applications |> 
+  # Calculate the annual rolling sum, starting with the most recent quarter
+  mutate(TotalQ = rollsum(Total, k = 4, na.pad = TRUE)) |> 
+  
+  # Keep the 2nd row (rolling sum for the past 12 months) and every 4th row after that
+  slice(seq(2, n(), by = 4)) |> 
+  arrange(desc(TotalQ)) |> 
+  
+  ggplot(aes(Date, TotalQ)) +
   geom_line(colour = brc_colours$red_dunant) +
-  geom_point(aes(size = Total, alpha = 0.5, colour = brc_colours$red_dunant), show.legend = FALSE) +
-  geom_text(aes(label = scales::comma(Total)), show.legend = FALSE, size = rel(2)) +
+  geom_point(aes(size = TotalQ, alpha = 0.5, colour = brc_colours$red_dunant), show.legend = FALSE) +
+  geom_text(aes(label = scales::comma(TotalQ)), show.legend = FALSE, size = rel(2)) +
   #geom_vline(xintercept = c(2002, 2022), size = 0.3, linetype = "dotted") +
   #annotate("text", x = 2002, y = 110000, label = "Conflicts in Afghanistan, Iran, Somalia and Sri Lanka", size = 2.5, hjust = -.01) +
   #annotate("text", x = 2022, y = 100000, label = "Conflicts in Ukraine and Afghanistan", size = 2.5) +
   theme_brc() +
-  scale_x_continuous(breaks = c(2001:2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 120000), expand = c(0, NA)) +
+  #scale_x_continuous(breaks = c(2001:2023)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA), expand = c(0, NA)) +
   labs(title = "Total number of asylum applications from 2001 to 2023", 
        x = "Year", 
        y = "Applications", 
-       caption = "British Red Cross analysis of Home Office data, March 2001 to March 2023") 
+       caption = "British Red Cross analysis of Home Office data, March 2001 to June 2023") 
 
 
-# ---- 2. Age and Sex Analysis 2023 Q1 Only ----
+# ---- 2. Age and Sex Analysis 2023 Q2 Only ----
 age_and_sex_analysis <- applications |>
   select(Date, Year, Age, Sex, Applications) |>
   filter(Year > '2008')
@@ -43,7 +51,8 @@ age_and_sex_analysis <- age_and_sex_analysis |>
   
 age_and_sex_analysis <- age_and_sex_analysis |>
   filter(Sex != "Unknown Sex") |>
-  filter(Age != "Unknown")
+  filter(Age != "Unknown") |>
+  filter(Age != "NA")
 
 # Adjusting the order of age
 age_and_sex_analysis$Age <- factor(age_and_sex_analysis$Age, levels=c('70+', '50-69', '30-49', '18-29', 'Under 18'))
@@ -54,11 +63,11 @@ age_and_sex_analysis |>
   geom_bar(position = "stack", stat = "identity") +
   #geom_text(aes(label = Total), position = position_stack(vjust = -0.25),size = rel(2)) + 
   theme_brc() +
-  labs(title = "Number of asylum applicants by age and sex from March 2022 to March 2023 ", 
+  labs(title = "Number of asylum applicants by age and sex over last 12 months ", 
        x = "Sex", 
        y = "Applications", 
-       caption = "British Red Cross analysis of Home Office data, March 2022 to March 2023") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, NA), expand = c(0,NA)) +
+       caption = "British Red Cross analysis of Home Office data, March 2022 to June 2023") +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 80000), expand = c(0,NA)) +
   scale_fill_manual(values = c(brc_colours$red_deep,
                                brc_colours$red_mercer,
                                brc_colours$red_light,
@@ -86,9 +95,9 @@ dependent_children |>
   labs(title = "Number of asylum applications with dependent children from 2009 to 2023", 
        x = "Year", 
        y = 'Applications', 
-       caption = 'British Red Cross analysis of Home Office data, March 2009 to March 2023') +
+       caption = 'British Red Cross analysis of Home Office data, March 2009 to June 2023') +
   scale_x_continuous(breaks = c(2009:2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 10000), expand = c(0, NA))
+  scale_y_continuous(labels = scales::comma, limits = c(0, 15000), expand = c(0, NA))
 
 # ---- 4. UASC ----
 UASC <- applications |>
@@ -108,14 +117,14 @@ UASCOnly |>
   ggplot(aes(x = Year, y = UASCTotal)) +
   geom_line(aes(colour = brc_colours$red_dunant), show.legend = FALSE) +
   geom_point(colour = brc_colours$red_dunant, alpha = 0.5, show.legend = FALSE) +
-  geom_text(aes(label = scales::comma(UASCTotal)), show.legend = FALSE, size = rel(4)) +
+  geom_text(aes(label = scales::comma(UASCTotal)), show.legend = FALSE, size = rel(3)) +
   theme_brc() +
-  scale_x_continuous(breaks = c(2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
+  scale_x_continuous(breaks = c(2006:2023)) +
   labs(title = "Applications for asylum by unaccompanied asylum seeking children (UASC) from 2006-2023", 
        x = "Year", 
        y = "Applications", 
-       caption = "British Red Cross analysis of Home Office data, March 2006 to March 2023") +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 6000), expand = c(0, NA))
+       caption = "British Red Cross analysis of Home Office data, March 2006 to June 2023") +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 7000), expand = c(0, NA))
 
 # ---- 5. UASC Nationality ----
 UASC <- UASC |>
@@ -135,11 +144,11 @@ UASC |>
   geom_col(fill = brc_colours$red_dunant) +
   geom_text(aes(label = scales::comma(TotalUASC)), show.legend = FALSE, size = rel(3),  position = position_dodge(width=1), vjust=-0.25, colour = brc_colours$black_shadow) +
   theme_brc() +
-  labs(title = "Number of asylum applications of unaccompanied asylum seeking children for year ending March 2023",
+  labs(title = "Number of asylum applications of unaccompanied asylum seeking children for year ending June 2023",
        subtitle =  "Top 10 nationalities with the highest applications",
        x = "Nationality", 
        y = 'Number of Children', 
-       caption = 'British Red Cross analysis of Home Office data, March 2022 to March 2023') +
+       caption = 'British Red Cross analysis of Home Office data, March 2022 to June 2023') +
   scale_y_continuous(labels = scales::comma, limits = c(0, 15000), expand = c(0,NA)) +
   theme(axis.text.x = element_text(angle = 75, vjust = 0.5, hjust=0.5))
 
@@ -159,9 +168,9 @@ application_by_nationality |>
        subtitle = "Top 5 nationalities with the highest number of asylum applications to the United Kingdom each year",
        x = "Year", 
        y = "Number of asylum applications", 
-       caption = "British Red Cross analysis of Home Office data, March 2001 to March 2023") +
+       caption = "British Red Cross analysis of Home Office data, March 2001 to June 2023") +
   scale_x_continuous(breaks = c(2001 : 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 50000), expand = c(0, NA)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 60000), expand = c(0, NA)) +
   theme(axis.text.x = element_text(vjust = 0.5, hjust=1)) +
   scale_fill_manual(values = c(brc_colours$red_dunant,
                                brc_colours$red_deep,
