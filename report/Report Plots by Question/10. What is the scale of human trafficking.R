de9e31fc90bfaf978679ba6cdda5eba34cb8cb0f
mkugan <131258713+mkugan@users.noreply.github.com>
2023-08-31 14:36:30 +0100

    Updated for Q2 data

--- report/Report Plots by Question/10. What is the scale of human trafficking.R
+++ report/Report Plots by Question/10. What is the scale of human trafficking.R
@@ -8,48 +8,52 @@ source("report/theme_brc.R")
 # ---- Referral to NRM: ----
 nrm_referrals <- 
   bind_rows(
+    asylum::nrm_referrals_2023_q2,
     asylum::nrm_referrals_2023_q1,
     asylum::nrm_referrals_2022_q4,
-    asylum::nrm_referrals_2022_q3,
-    asylum::nrm_referrals_2022_q2
+    asylum::nrm_referrals_2022_q3
   )
 
-view(nrm_referrals %>%
-  group_by(Nationality) %>%
+view(nrm_referrals |>
+  group_by(Nationality) |>
   summarise(Total = sum(People)))
 
-NRMTotal <- nrm_referrals %>%
-  group_by(`Age at exploitation`, Gender, Nationality) %>%
+NRMTotal <- nrm_referrals |>
+  group_by(`Age at exploitation`, Gender, Nationality) |>
   summarise(Total = sum(People))
 
-NRMTotal <- NRMTotal %>%
-  filter(`Age at exploitation` != "Total") %>%
-  filter(Gender != "Total") %>%
+NRMTotal <- NRMTotal |>
+  filter(`Age at exploitation` != "Total") |>
+  filter(Gender != "Total") |>
   filter(Nationality != "Total")
 
 # "Total" was removed from each category as it was grouped for other analyses.
 
 # ---- 1. NRM by Top Nationalities ---- 
-NRMTotal %>%
-  filter(Nationality == "UK") %>%
-  group_by(Nationality) %>%
-  summarise(UK = sum(Total))
+NRMTotal |>
+  group_by(Nationality) |>
+  summarise(TotalNat = sum(Total)) |> view()
 
-# 1775 male children/ 2469 were referred to NRM- mostly male children from UK.
+# Total number of people = 13763
+
+# Total M Under 18 = 4841/13763
+
+# 70% of those referred to NRM is from Albania, the UK, Eritrea, Sudan, Vietnam and Iran. 
+
 
 NRMTotal |>
-  group_by(Nationality) %>%
-  summarise(TotalN = sum(Total)) %>%
-  filter(TotalN > 300) %>%
+  group_by(Nationality) |>
+  summarise(TotalN = sum(Total)) |>
+  slice_max(TotalN, n = 10) |>
   ggplot(aes(x = reorder(Nationality, desc(TotalN)), y = TotalN)) +
   geom_col(fill = brc_colours$red_dunant) +
   geom_text(aes(label = scales::comma(TotalN)), show.legend = FALSE, size = rel(3),  position = position_dodge(width=1), vjust=-0.25, colour = brc_colours$black_shadow) +
   theme_brc() +
-  labs(title = str_wrap("Nationalities with the largest number of referrals to the National Referral Mechanism (NRM) processed for year ending March 2023"),
-       subtitle =  "People from the United Kingdom, Albania, Eritrea, Iran, Sudan and Vietnam made up 70% of all NRM referrals",
+  labs(title = str_wrap("Nationalities with the largest number of referrals to the National Referral Mechanism (NRM) processed for year ending June 2023"),
+       subtitle =  "People from Albania, the United Kingdom, Eritrea, Sudan, Vietnam and Iran made up 70% of all NRM referrals",
        x = "Nationality", 
        y = 'Number of people', 
-       caption = 'British Red Cross analysis of Home Office data, March 2022 to March 2023') +
+       caption = 'British Red Cross analysis of Home Office data, June 2022 to June 2023') +
   scale_y_continuous(labels = scales::comma, limits = c(0, 5000), expand = c(0, NA)) +
   theme(axis.text.x = element_text(angle = 80, vjust = 0.5, hjust=0.5))
 
@@ -64,43 +68,43 @@ NRMTotal %>%
   geom_bar(position = "stack", stat ="identity") +
   geom_text(aes(label = scales::comma(TotalSum)), position = position_stack(vjust = .5),size = rel(3)) +
   theme_brc() +
-  labs(title = strwrap("Number of referrals to the National Referral Mechanism (NRM) by age and sex for year ending March 2023"), 
+  labs(title = strwrap("Number of referrals to the National Referral Mechanism (NRM) by age and sex for year ending June 2023"), 
        x = "Sex", 
        y = "Number of Referrals",
-       caption = "British Red Cross analysis of Home Office data, March 2022 to March 2023") +
+       caption = "British Red Cross analysis of Home Office data, June 2022 to June 2023") +
   scale_y_continuous(labels = scales::comma, limits = c(0, 12000), expand = c(0, NA)) +
-  scale_fill_manual(values = c(brc_colours$red_dunant,
-                               brc_colours$red_earth))
+  scale_fill_manual(values = c(brc_colours$red_light,
+                               brc_colours$red_dunant))
  
 # ---- 3. NRM Duty to Notify ----
 view(nrm_duty_to_notify_2022_q2)
 
 nrm_duty_to_notify <- bind_rows(
+  asylum::nrm_duty_to_notify_2023_q2,
   asylum::nrm_duty_to_notify_2023_q1,
   asylum::nrm_duty_to_notify_2022_q4,
-  asylum::nrm_duty_to_notify_2022_q3,
-  asylum::nrm_duty_to_notify_2022_q2
+  asylum::nrm_duty_to_notify_2022_q3
 )
 
-DTNTotal <- nrm_duty_to_notify %>%
-  group_by(Nationality) %>%
+DTNTotal <- nrm_duty_to_notify |>
+  group_by(Nationality) Z>
   filter(Nationality != "Total")
 
-DTNTotal <- DTNTotal %>%
-  group_by(Nationality) %>%
+DTNTotal <- DTNTotal |>
+  group_by(Nationality) |>
   summarise(Total2 = sum(Total))
 
-DTNTotal %>%
-  filter(Total2 > 200) %>%
+DTNTotal |>
+  slice_max(Total2, n = 10) |>
   ggplot(aes(x = reorder(Nationality, desc(Total2)), y = Total2)) +
   geom_col(fill = brc_colours$red_dunant, colour = brc_colours$red_dunant) +
   geom_text(aes(label = scales::comma(Total2)), show.legend = FALSE, size = rel(3),  position = position_dodge(width=1), vjust=-0.25, colour = brc_colours$black_shadow) +
   theme_brc() +
-  labs(title = str_wrap("Nationalities with the largest number of reports via the Duty to Notify process for year ending March 2023"),
+  labs(title = str_wrap("Nationalities with the largest number of reports via the Duty to Notify process for year ending June 2023"),
        # subtitle = "406 individuals from the United Kingdom were referred to Duty to Notify from April 2022 to January 2023",
        x = "Nationalities", 
        y = "Number of reports", 
-       caption = "British Red Cross analysis of Home Office data, March 2022 to March 2023") +
+       caption = "British Red Cross analysis of Home Office data, June 2022 to June 2023") +
   scale_y_continuous(labels = scales::comma, limits = c(0, 1000), expand = c(0,NA))
 
 # ---- 4. NRM Reasonable Grounds ---- 
@@ -135,13 +139,11 @@ nrm_reasonable_grounds |>
        x = "Year", 
        y = "Number of people",
        fill = "Reasonable grounds decision",
-       caption = "British Red Cross analysis of Home Office data, March 2014 to March 2023") +
+       caption = "British Red Cross analysis of Home Office data, March 2014 to June 2023") +
   scale_x_continuous(breaks = c(2014:2023)) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 20000), expand = c(0,NA)) +
-  scale_fill_manual(values = c(brc_colours$red_deep,
-                               brc_colours$red_dunant))
-
-#check with Matt on why this doesnt work as well.
+  scale_fill_manual(values = c(brc_colours$red_light,
+                               brc_colours$red_mercer))
 
 
 # ---- 5. Positive Conclusive Grounds ---- 
@@ -171,8 +173,8 @@ nrm_conclusive_grounds |>
   labs(title = "Proportion of people who received positive conclusive grounds by age from 2014 to 2023", 
        x = "Year", 
        y = "Number of people",
-       caption = "British Red Cross analysis of Home Office data, March 2014 to March 2023") +
-  scale_x_continuous(breaks = c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023)) +
-  scale_y_continuous(labels = scales::comma, limits = c(0, 6000), expand = c(0,NA)) +
-  scale_fill_manual(values = c(brc_colours$red_deep,
-                               brc_colours$red_dunant))
+       caption = "British Red Cross analysis of Home Office data, March 2014 to June 2023") +
+  scale_x_continuous(breaks = c(2014:2023)) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 7000), expand = c(0,NA)) +
+  scale_fill_manual(values = c(brc_colours$red_light,
+                               brc_colours$red_mercer))
