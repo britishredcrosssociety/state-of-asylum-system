7ab788d2a70a7100da51242ffa9b498fa1c3409f
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-12-02 17:25:49 +0000

    Analyse and plot asylum claims

---
+++ sotn-website/brc_colours.R
@@ -0,0 +1,32 @@
+# British Red Cross colour palette
+brc_colours <- list(
+  # Hero colour
+  red_dunant = "#ee2a24",
+  
+  # Primary palette
+  white_clarity = "#ffffff",
+  black_shadow = "#1d1a1c",
+  black_full = "#000000",
+  
+  # Hero colour - tones
+  red_earth = "#9d1f21",
+  red_deep = "#651713",
+  red_light = "#e95351",
+  red_mercer = "#d0011b",
+  
+  # Secondary palette
+  green = "#40a22a",
+  green_dark = "#05853a",
+  blue = "#193351",
+  teal = "#287286",
+  sky = "#158aba",
+  steel = "#5a98c0",
+  duck = "#baddea",
+  white_flag = "#f6f6f6",
+  grey_fog = "#d7d8d7",
+  grey = "#5c747a",
+  earth = "#afa48f",
+  sand = "#e4d7aa",
+  mustard = "#f1b13b",
+  claret = "#7d1c23"
+)
---
+++ sotn-website/theme_brc.R
@@ -0,0 +1,49 @@
+theme_brc <- function(...) {
+  # font <- "Arial"
+  font <- NULL
+  
+  ggplot2::theme(
+    # Plot title
+    plot.title.position = "plot",
+    plot.title = ggplot2::element_text(family = font,
+                                       size = 16,
+                                       face = "bold",
+                                       color = brc_colours$black_shadow),
+    
+    plot.subtitle = ggplot2::element_text(family = font,
+                                          size = 12,
+                                          color = brc_colours$black_shadow),
+    
+    plot.caption.position = "plot",
+    plot.caption = ggplot2::element_text(family = font,
+                                         size = ggplot2::rel(0.8),
+                                         color = brc_colours$black_shadow,
+                                         hjust = 0),
+    
+    axis.text = ggplot2::element_text(family = font, size = ggplot2::rel(0.8), colour = brc_colours$black_shadow),
+    axis.title = ggplot2::element_text(family = font, face = "bold", colour = brc_colours$black_shadow),
+    
+    # no background
+    panel.border = ggplot2::element_blank(),
+    panel.background = ggplot2::element_rect(fill = "white", colour = NA),
+    
+    # make gridlines dark, same contrast with white as in theme_grey
+    panel.grid = ggplot2::element_line(colour = "grey92"),
+    panel.grid.major = ggplot2::element_line(size = ggplot2::rel(0.8)),
+    panel.grid.minor = ggplot2::element_line(size = ggplot2::rel(0.4)),
+    
+    # show axes
+    axis.line = ggplot2::element_line(colour = brc_colours$black_shadow, size = ggplot2::rel(1)),
+    
+    # match legend key to panel.background
+    legend.key = ggplot2::element_blank(),
+    
+    # simple, black and white strips for facets
+    strip.background = ggplot2::element_rect(fill = "white", colour = brc_colours$black_shadow, size = ggplot2::rel(2)),
+    strip.text = ggplot2::element_text(family = font, hjust = 0),
+    
+    legend.position = "top",
+    
+    ...
+  )
+}
